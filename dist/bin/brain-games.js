1a90f7daa538636643d6dae3a028b86a67ce9b9f
Koshkarik <koshkarevj@mail.ru>
2018-01-01 20:44:12 +0300

    upd

--- dist/bin/brain-games.js
+++ dist/bin/brain-games.js
@@ -1,5 +1,7 @@
 #!/usr/local/bin/node
 'use strict';
 
-console.log('Welcome to the Brain Games!');
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6Ijs7QUFDQUEsUUFBUUMsR0FBUixDQUFZLDZCQUFaIiwiZmlsZSI6ImJyYWluLWdhbWVzLmpzIiwic291cmNlc0NvbnRlbnQiOlsiXG5jb25zb2xlLmxvZygnV2VsY29tZSB0byB0aGUgQnJhaW4gR2FtZXMhJyk7Il19
+var _ = require('..');
+
+(0, _.sayHello)();
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFDQTs7QUFFQSIsImZpbGUiOiJicmFpbi1nYW1lcy5qcyIsInNvdXJjZXNDb250ZW50IjpbIlxuaW1wb3J0IHsgc2F5SGVsbG8gfSBmcm9tICcuLic7XG5cbnNheUhlbGxvKCk7Il19
--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,4 @@
 #!/usr/local/bin/node
-console.log('Welcome to the Brain Games!');
+import { sayHello } from '..';
+
+sayHello();
