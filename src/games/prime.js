5c4cdd7314e0dfe68146d284feb6136a7abcd4a4
Koshkarik <koshkarevj@mail.ru>
2018-01-06 21:12:25 +0300

    final changes

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,13 +5,13 @@ const message = 'Is this number prime?';
 const isPrime = (numb) => {
   const checker = Math.floor(numb / 2);
   if (numb === 0 || numb === 1) return false;
-  function findRecursive(number, checkNum) {
+  function primeCheck(number, checkNum) {
     if (checkNum <= 2) return true;
     if (numb % checkNum === 0) return false;
     const newCheck = checkNum - 1;
-    return findRecursive(numb, newCheck);
+    return primeCheck(numb, newCheck);
   }
-  return findRecursive(numb, checker);
+  return primeCheck(numb, checker);
 };
 
 const makePrimeGame = () => {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import readlineSync from 'readline-sync';
 
-// still need to export those functions for braingames.js in bin
 export const welcomeMsg = () => {
   console.log('Welcome to the Brain Games!');
 };
@@ -15,6 +14,7 @@ export const sayHello = () => {
 // generates random number between 1 and 100
 export const giveRandNumb = () => Math.floor((Math.random() * 100) + 1);
 
+// generates random number between 1 and 10
 export const giveSmRandNumb = () => Math.floor((Math.random() * 10) + 1);
 
 // asks your desired question before game begins
@@ -26,11 +26,7 @@ const userAnswer = () => readlineSync.question('Your answer: ');
 // takes wrong and correct answer and returns console.log with those values
 const explainWrongAnswer = (answer, correctAnswer) => console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
 
-// makes fixed length array with random numbers
-// const makeArrWithRandomNumbers = num => Array(num).fill(null).map(item => giveRandNumb(item));
-
-// our gameFunc should return object
-// {message: message before the game, question: task(string), correctAnswer }
+// main function - game generator
 export const makeGame = (gameFunc, message) => {
   let attempt = 0;
   welcomeMsg();
@@ -51,9 +47,3 @@ export const makeGame = (gameFunc, message) => {
   }
   console.log(`Congratulations, ${name}!`);
 };
-
-// const gameBuilder = makeGameFunc => (game, message) => () => makeGameFunc(game, message);
-
-// in your game section put game as first argument, task message as second argument,
-// quantity of random numbers needed for game function, than export it to bin file and call it!
-// export const generateGame = gameBuilder(makeGame);
